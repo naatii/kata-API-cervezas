686dc2e2871ec81ae5569e852b2cac69d807a0df
RekZNoZ <rafikicroz@hotmail.com>
2024-11-09 12:27:00 +0100

    Update BreweryController.java

--- src/main/java/daw2a/kataapicervezas/controllers/BreweryController.java
+++ src/main/java/daw2a/kataapicervezas/controllers/BreweryController.java
@@ -1,24 +1,59 @@
 package daw2a.kataapicervezas.controllers;
 
+import daw2a.kataapicervezas.entities.Brewery;
+import daw2a.kataapicervezas.service.BreweryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/brewery")
 public class BreweryController {
+
+    @Autowired
+    private final BreweryService breweryService;
+    public BreweryController(BreweryService breweryService) {
+        this.breweryService = breweryService;
+    }
+
     @GetMapping
-    public String getBrewery() {
-        return "Brewery";
+    public ResponseEntity<List<Brewery>> listarCervecerias() {
+        List<Brewery> breweries = breweryService.listarCervecerias();
+        return ResponseEntity.ok(breweries);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Brewery> obtenerCerveceria(@PathVariable Long id){
+        return breweryService.obtenerCerveceria(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
     @PostMapping
-    public String postBrewery() {
-        return "Brewery";
+    public ResponseEntity<Brewery> agregarCerveceria(@RequestBody @Valid Brewery brewery) {
+        Brewery nuevaBrewery = breweryService.agregarCerveceria(brewery);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaBrewery);
     }
-    @DeleteMapping
-    public String deleteBrewery() {
-        return "Brewery";
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Brewery> borrarCerveceria(@PathVariable Long id) {
+        try {
+            breweryService.borrarCerveceria(id);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
-    @PutMapping
-    public String putBrewery() {
-        return "Brewery";
+    @PutMapping("/{id}")
+    public ResponseEntity<Brewery> actualizarCerveceria(@PathVariable Long id, @RequestBody @Valid Brewery brewery) {
+        try {
+            Brewery breweryActualizada = breweryService.actualizarCerveceria(id, brewery);
+            return ResponseEntity.ok(breweryActualizada);
+        } catch (NoSuchElementException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
+
 }
