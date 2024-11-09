b7337e6b20b08e625f5c8ecdf7c80f9e0ec6abfb
RekZNoZ <rafikicroz@hotmail.com>
2024-11-09 13:38:52 +0100

    Update BreweryControllerTest.java

--- src/test/java/daw2a/kataapicervezas/controllers/BreweryControllerTest.java
+++ src/test/java/daw2a/kataapicervezas/controllers/BreweryControllerTest.java
@@ -100,10 +100,8 @@ public class BreweryControllerTest {
         ResponseEntity<Brewery> response = breweryController.borrarCerveceria(1L);
 
         // Assert
-        assertEquals(Optional.empty(), breweryService.obtenerCerveceria(1L));
         assertEquals(204, response.getStatusCode().value());
         verify(breweryService, times(1)).borrarCerveceria(1L);
-        verify(breweryService, times(1)).obtenerCerveceria(1L);
     }
 
     @Test
