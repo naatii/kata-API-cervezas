ccf4713238287728d03c8eb24d370596d3977d51
RekZNoZ <rafikicroz@hotmail.com>
2024-11-09 12:01:41 +0100

    Update BreweryRepository.java

--- src/main/java/daw2a/kataapicervezas/repositories/BreweryRepository.java
+++ src/main/java/daw2a/kataapicervezas/repositories/BreweryRepository.java
@@ -1,9 +1,9 @@
 package daw2a.kataapicervezas.repositories;
 
 import daw2a.kataapicervezas.entities.Brewery;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BreweryRepository extends CrudRepository<Brewery, Long> {
+public interface BreweryRepository extends JpaRepository<Brewery, Long> {
 }
